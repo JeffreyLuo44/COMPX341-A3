b4bf46a8c8e3dd1ac54a3ccdc23e7e246dfb8ecc
jeffrey <jeffrey.luo440@gmail.com>
2022-05-15 17:03:13 +1200

    COMPX341-22A Custom commit message from CI/CD Pipeline

--- assets/pipeline.ps1
+++ assets/pipeline.ps1
@@ -2,11 +2,17 @@ npm install
 npm run build
 #Check if there are no compile-time errors from building code
 if ($? -eq $True){
-  cd ..
-  git add .
-  git commit -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
-  git push origin master
-  npm run start
+  #Check if there is an argument for the custom commit message
+  if ($args[0] -ne $null){
+    cd ..
+    git add .
+    git commit -m $args[0]
+    git push
+    cd assets
+    npm run start
+  }else{
+    Write-Host("Please specify a custom commit message as an argument!")
+  }
 }else{
   Write-Host "There are compile-time errors so commit/push is not allowed!"
 }
